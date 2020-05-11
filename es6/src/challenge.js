40ee15d658e8818b7ac084ab289dd56e977f640e
bernecg <bernard.caye@gmail.com>
2020-05-11 19:38:25 -0300

    Finish ES6+ module with app to fetch GitHub repo info

--- es6/src/challenge.js
+++ es6/src/challenge.js
@@ -17,9 +17,18 @@ const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
 // }
 
 const umPorSegundo = async() => {
-    await delay().then(() => console.log("1s"));
-    await delay().then(() => console.log("2s"));
-    await delay().then(() => console.log("3s"));
+    // await delay().then(() => console.log("1s"));
+    // await delay().then(() => console.log("2s"));
+    // await delay().then(() => console.log("3s"));
+    await delay();
+    console.log("1s");
+
+    await delay();
+    console.log("2s");
+
+    await delay();
+    console.log("3s");
+
 }
 
 umPorSegundo();
--- es6/src/main.js
+++ es6/src/main.js
@@ -1,1 +1,89 @@
+import api from "./github_api";
+
+class App {
+    constructor() {
+        this.repositories = [];
+        this.formElem = document.getElementById("repo-form");
+        this.inputElem = document.querySelector("input[name=repository]");
+        this.uListElem = document.getElementById("repo-list");
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formElem.onclick = (event) => this.addRepository(event);
+    }
+
+    setLoading(loading = true) {
+        if (loading === true) {
+            let loadingEle = document.createElement("span");
+            loadingEle.appendChild(document.createTextNode("Loading..."));
+            loadingEle.setAttribute("id", "loading");
+
+            this.formElem.appendChild(loadingEle);
+        } else {
+            document.getElementById("loading").remove();
+        }
+    }
+
+    async addRepository(event) {
+        event.preventDefault();
+        let repoName = this.inputElem.value;
+
+        if (repoName.length === 0) {
+            return;
+        }
+
+        this.setLoading();
+
+        try {
+            const responseRepo = await api.get(`/repos/${repoName}`);
+
+            const { name, description, html_url, owner: { avatar_url } } = responseRepo.data;
+
+            this.repositories.push({ name, description, avatar_url, html_url });
+
+            this.inputElem.value = "";
+
+            this.renderRepos();
+        }
+        catch (err) {
+            alert(`Repo ${repoName} does not exists...`);
+        }
+
+        this.setLoading(false);
+    }
+
+    renderRepos() {
+        this.uListElem.innerHTML = "";
+        this.repositories.forEach(repo => {
+            let imgEle = document.createElement("img");
+            imgEle.setAttribute("src", repo.avatar_url);
+            let aImgEle = document.createElement("a");
+            aImgEle.setAttribute("target", "_blanket");
+            aImgEle.setAttribute("href", repo.html_url);
+            aImgEle.appendChild(imgEle);
+
+            let strongEle = document.createElement("strong");
+            strongEle.appendChild(document.createTextNode(repo.name));
+
+            let pEle = document.createElement("p");
+            pEle.appendChild(document.createTextNode(repo.description));
+
+            let aEle = document.createElement("a");
+            aEle.setAttribute("target", "_blanket");
+            aEle.setAttribute("href", repo.html_url);
+            aEle.appendChild(document.createTextNode("Access"));
+
+            let liEle = document.createElement("li");
+            liEle.appendChild(aImgEle);
+            liEle.appendChild(strongEle);
+            liEle.appendChild(pEle);
+            liEle.appendChild(aEle);
+
+            this.uListElem.appendChild(liEle);
+        });
+    }
+}
+
+new App();
 console.log("App loaded...");
